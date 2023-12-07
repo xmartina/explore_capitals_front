4a03c4db97a5c8647f5e27bcac5860eee53e7349
xmartina <martinsprince126@gmail.com>
2023-12-07 18:12:50 +0100

    d

--- index.php
+++ index.php
@@ -3,5 +3,24 @@ require_once ('partials/header.php'); ?>
 
 <main id="main" class="main-img">
 
+    <?php
+        require_once ('home/banner.php');
+        require_once ('home/calculator.php');
+        require_once ('home/investment.php');
+        require_once ('home/under-investment-modal.php');
+        require_once ('home/how-it-work.php');
+        require_once ('home/about-us.php');
+        require_once ('home/why-choose-us.php');
+        require_once ('home/faq.php');
+        require_once ('home/top-investor.php');
+        require_once ('home/recent-blog.php');
+        require_once ('home/testimonier.php');
+        require_once ('home/recent-blog.php');
+        require_once ('home/newsletter.php');
+        require_once ('home/profit-calculator-modal.php');
+
+
+    ?>
+
 </main>
 <?php require_once ('partials/footer.php');
