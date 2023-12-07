a63978d25062c1b1cc49f045b04ac1eb0d05d26e
xmartina <martinsprince126@gmail.com>
2023-12-07 18:31:26 +0100

    d

--- include/config.php
+++ include/config.php
@@ -3,5 +3,5 @@
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
 
-const siteName = "Explore Capitals";
-const siteTag = "Best investment Option";
+//const siteName = "Explore Capitals";
+//const siteTag = "Best investment Option";
