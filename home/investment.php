6525cd4172597bcbe437e9ee47d8edf49be5fef4
xmartina <martinsprince126@gmail.com>
2023-12-07 17:50:38 +0100

    d

---
+++ home/investment.php
@@ -0,0 +1,196 @@
+<section class="s-pt-100 s-pb-100 dark-bg">
+    <div class="container">
+
+        <div class="row justify-content-center">
+            <div class="col-lg-6 text-center">
+                <div class="site-header">
+                    <h2 class="site-title">Investment plan</h2>
+                </div>
+            </div>
+        </div>
+
+        <div class="row gy-4">
+            <div class="col-lg-4 col-md-6">
+                <div class="invest-plan">
+                    <div class="invest-plan-shape"></div>
+                    <div class="invest-plan-top">
+                        <h4 class="invest-plan-name">Hourly</h4>
+                        <h5 class="invest-plan-amount">10.00 $
+                        </h5>
+                        <p class="mb-0">Every ROI Time</p>
+                    </div>
+
+                    <div class="invest-plan-middle">
+                        <h5 class="invest-plan-min-max">
+                            1.00 $
+                        </h5>
+                        <ul class="invest-plan-features">
+                            <li>
+
+                                Lifetime
+
+                            </li>
+
+
+                            <li>Capital back No</li>
+                        </ul>
+
+                        <h6 class="mt-4 mb-3">Affiliate bonus</h6>
+                        <ul class="plan-referral justify-content-center">
+                            <div class="single-referral">
+                                <span>1 %</span>
+                                <p>level 1</p>
+                            </div>
+                            <div class="single-referral">
+                                <span>2 %</span>
+                                <p>level 2</p>
+                            </div>
+                            <div class="single-referral">
+                                <span>3 %</span>
+                                <p>level 3</p>
+                            </div>
+                        </ul>
+                    </div>
+                    <div class="invest-plan-action mt-3">
+                        <a class="sp_theme_btn w-100 mb-3" href="login.html">Invest now</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="invest-plan">
+                    <div class="invest-plan-shape"></div>
+                    <div class="invest-plan-top">
+                        <h4 class="invest-plan-name">Prince</h4>
+                        <h5 class="invest-plan-amount">33.00 %
+                        </h5>
+                        <p class="mb-0">Every 6 Month</p>
+                    </div>
+
+                    <div class="invest-plan-middle">
+                        <h5 class="invest-plan-min-max">
+                            Min
+                            22.00 $
+                            <p class="mb-0">-</p>
+                            Max
+                            33.00 $
+                        </h5>
+                        <ul class="invest-plan-features">
+                            <li>
+
+                                Lifetime
+
+                            </li>
+
+
+                            <li>Capital back No</li>
+                        </ul>
+
+                        <h6 class="mt-4 mb-3">Affiliate bonus</h6>
+                        <ul class="plan-referral justify-content-center">
+                        </ul>
+                    </div>
+                    <div class="invest-plan-action mt-3">
+                        <a class="sp_theme_btn w-100 mb-3" href="login.html">Invest now</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="invest-plan">
+                    <div class="invest-plan-shape"></div>
+                    <div class="invest-plan-top">
+                        <h4 class="invest-plan-name">Queen</h4>
+                        <h5 class="invest-plan-amount">3.00 %
+                        </h5>
+                        <p class="mb-0">Every 6 Month</p>
+                    </div>
+
+                    <div class="invest-plan-middle">
+                        <h5 class="invest-plan-min-max">
+                            Min
+                            1.00 $
+                            <p class="mb-0">-</p>
+                            Max
+                            10.00 $
+                        </h5>
+                        <ul class="invest-plan-features">
+                            <li>
+
+                                For 44
+                                Times
+
+                            </li>
+
+
+                            <li>Capital back Yes</li>
+                        </ul>
+
+                        <h6 class="mt-4 mb-3">Affiliate bonus</h6>
+                        <ul class="plan-referral justify-content-center">
+                        </ul>
+                    </div>
+                    <div class="invest-plan-action mt-3">
+                        <a class="sp_theme_btn w-100 mb-3" href="login.html">Invest now</a>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="invest-plan">
+                    <div class="invest-plan-shape"></div>
+                    <div class="invest-plan-top">
+                        <h4 class="invest-plan-name">King</h4>
+                        <h5 class="invest-plan-amount">2.00 %
+                        </h5>
+                        <p class="mb-0">Every 6 Month</p>
+                    </div>
+
+                    <div class="invest-plan-middle">
+                        <h5 class="invest-plan-min-max">
+                            Min
+                            100.00 $
+                            <p class="mb-0">-</p>
+                            Max
+                            1,000.00 $
+                        </h5>
+                        <ul class="invest-plan-features">
+                            <li>
+
+                                Lifetime
+
+                            </li>
+
+
+                            <li>Capital back No</li>
+                        </ul>
+
+                        <h6 class="mt-4 mb-3">Affiliate bonus</h6>
+                        <ul class="plan-referral justify-content-center">
+                            <div class="single-referral">
+                                <span>1 %</span>
+                                <p>level 1</p>
+                            </div>
+                            <div class="single-referral">
+                                <span>2 %</span>
+                                <p>level 2</p>
+                            </div>
+                            <div class="single-referral">
+                                <span>3 %</span>
+                                <p>level 3</p>
+                            </div>
+                            <div class="single-referral">
+                                <span>4 %</span>
+                                <p>level 4</p>
+                            </div>
+                        </ul>
+                    </div>
+                    <div class="invest-plan-action mt-3">
+                        <a class="sp_theme_btn w-100 mb-3" href="login.html">Invest now</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
