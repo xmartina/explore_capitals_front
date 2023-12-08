48b17883522cda0d0ae060a9bd646fa41b1a2463
xmartina <martinsprince126@gmail.com>
2023-12-08 09:24:17 +0100

    d

--- contact/form-success.php
+++ contact/form-success.php
@@ -4,6 +4,7 @@ const rootDir = "/home/multistream6/domains/explorecapitals.online/public_html/"
 require_once (rootDir.'include/config.php');
 require_once (rootDir.'partials/header.php');
 require_once (rootDir.'contact/breadcrumbs.php');
+require_once (rootDir.'contact/form-success-body.php');
 
 
 
