afb375ce5e7b52782f3f368416f1250397891a65
xmartina <martinsprince126@gmail.com>
2023-12-08 08:28:09 +0100

    d

--- investment/plan/index.php
+++ investment/plan/index.php
@@ -3,9 +3,9 @@ const pageName = "Investment Plans";
 const rootDir = "/home/multistream6/domains/explorecapitals.online/public_html/";
 require_once (rootDir.'include/config.php');
 require_once (rootDir.'partials/header.php');
-require_once ('breadcrumbs.php');
-require_once ('investment-plans.php');
-require_once ('modal.php');
+require_once (rootDir.'investment/plan/breadcrumbs.php');
+require_once (rootDir.'investment/plan/investment-plans.php');
+require_once (rootDir.'investment/plan/modal.php');
 
 
 require_once (rootDir.'partials/footer.php');
