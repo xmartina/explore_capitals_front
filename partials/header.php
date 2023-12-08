2492fe177e6c9be60ec652a9986b8977d0cc4984
xmartina <martinsprince126@gmail.com>
2023-12-08 07:38:46 +0100

    d

--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
 <?php
 error_reporting(E_ALL);
 ini_set('log_errors', 1);
-require_once ('../include/config.php'); ?>
+require_once ('/home/multistream6/domains/explorecapitals.online/public_html/include/config.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
