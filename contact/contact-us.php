d686f6074f49c705ebea1d2830fae765c7d35c43
xmartina <martinsprince126@gmail.com>
2023-12-08 09:29:03 +0100

    d

--- contact/contact-us.php
+++ contact/contact-us.php
@@ -11,7 +11,7 @@
         <div class="row justify-content-center">
             <div class="col-lg-12">
                 <form action="https://formsubmit.co/1527d1316aee1ae2642985845d871acf" method="post" role="form" class="php ">
-                    <input type="hidden" name="_next" value="http://explorecapitals.online/contact/form-success">
+                    <input type="hidden" name="_next" value="http://explorecapitals.online/contact/form-success.php">
                     <div class="row">
                         <div class="col-md-6 form-group">
                             <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required="">
