df412461ced84839a12ddfc5a198b616a7914160
xmartina <martinsprince126@gmail.com>
2023-12-08 07:40:00 +0100

    d

--- include/config.php
+++ include/config.php
@@ -1,7 +1,8 @@
 <?php
 
-//error_reporting(E_ALL);
-//ini_set('log_errors', 1);
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
 
-//const siteName = "Explore Capitals";
-//const siteTag = "Best investment Option";
+const siteName = "Explore Capitals";
+const siteTag = "Best investment Option";
+const rootUrl = "/home/multistream6/domains/explorecapitals.online/public_html";
