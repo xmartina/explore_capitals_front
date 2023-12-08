bae572e5f05b2655832f4797c7f69f2a4e511739
xmartina <martinsprince126@gmail.com>
2023-12-08 07:29:13 +0100

    d

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,7 @@
-<?php require_once ('../include/config.php'); ?>
+<?php
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+require_once ('../include/config.php'); ?>
 
 <!DOCTYPE html>
 <html lang="en">
