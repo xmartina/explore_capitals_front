64a554c457fe27db9f41b4c8f0ed1a7bd79755a9
xmartina <martinsprince126@gmail.com>
2023-12-09 12:29:29 +0100

    d

--- investment/breadcrumbs.php
+++ investment/breadcrumbs.php
@@ -1,7 +1,7 @@
 <section class="breadcrumbs" style="background-image: url(/asset/theme1/images/breadcrumbs/breadcrumbs.jpg);">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center text-capitalize">
-            <h2>Investment plan</h2>
+            <h2>Investment plans</h2>
             <ol>
                 <li><a href="/">Home</a></li>
                 <li>Investment plan</li>
