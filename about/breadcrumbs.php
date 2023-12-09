d58f57053c7af18268fbb5f5e6efbb73ae959517
xmartina <martinsprince126@gmail.com>
2023-12-09 12:46:36 +0100

    d

--- about/breadcrumbs.php
+++ about/breadcrumbs.php
@@ -1,5 +1,5 @@
 <!-- ======= Breadcrumbs ======= -->
-<section class="breadcrumbs" style="background-image: url(asset/theme1/images/breadcrumbs/breadcrumbs.jpg);">
+<section class="breadcrumbs" style="background-image: url(/asset/theme1/images/breadcrumbs/breadcrumbs.jpg);">
     <div class="container">
         <div class="d-flex justify-content-between align-items-center text-capitalize">
             <h2>About</h2>
