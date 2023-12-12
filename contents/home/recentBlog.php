4e7e5ee2843cd3335621a0cf0c17e5b517ca25b9
xmartina <martinsprince126@gmail.com>
2023-12-12 13:26:13 +0100

    d

--- contents/home/recentBlog.php
+++ contents/home/recentBlog.php
@@ -7,6 +7,11 @@ class recentBlog
     public function __construct(){
         $p = $this;
 
-        $p->bh1 = '';
+        $p->bh1 = 'Navigating the Crypto Boom: Key Trends in 2023';
+        $p->bc1 = 'Content: Explore rising altcoins, decentralized finance, and the latest regulatory developments reshaping the crypto landscape.';
+        $p->bh2 = "The Metaverse Wave: Crypto's Next Frontier";
+        $p->bc2 = 'Content: Uncover how blockchain technology is driving the evolution of the metaverse and its potential impact on crypto markets.';
+        $p->bh3 = 'Sustainability in Crypto: Green Initiatives Taking Root';
+        $p->bc3 = 'Content: Discover how the crypto industry is embracing eco-friendly solutions, from energy-efficient consensus algorithms to carbon offset initiatives.';
     }
 }
--- home/recent-blog.php
+++ home/recent-blog.php
@@ -1,3 +1,7 @@
+<?php
+use contents\home\recentBlog;
+$recentBlog = new recentBlog();
+?>
 <section class="s-pt-100 s-pb-100 dark-bg">
 
     <div class="container">
@@ -10,59 +14,53 @@
         </div>
         <div class="row gy-4">
             <div class="col-md-6 col-lg-4">
-                <div class="blog-box">
-                    <div class="blog-box-thumb">
-                        <img src="asset/theme1/images/blog/624d61e797df71649238503.jpg" alt="image">
+                <div class="/blog-box">
+                    <div class="/blog-box-thumb">
+                        <img src="/asset/theme1/images/blog/624d61e797df71649238503.jpg" alt="image">
                     </div>
-                    <div class="blog-box-content">
-                        <span class="blog-category">Bitcoin</span>
-                        <h3 class="title"><a href="blog/31/similique-totam-harum-rerum.html">Similique totam harum
-                                rerum.</a>
+                    <div class="/blog-box-content">
+                        <span class="/blog-category">Bitcoin</span>
+                        <h3 class="title"><a href="/blog/31/similique-totam-harum-rerum.html"><?=$recentBlog->bh1?></a>
                         </h3>
-                        <ul class="blog-meta">
+                        <ul class="/blog-meta">
                             <li><i class="fas fa-clock"></i> 1 year ago</li>
                             <li><i class="fas fa-comment"></i> 0 Comments</li>
                         </ul>
-                        <p class="mb-0 mt-3">Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting
-                            Industry. Lorem Ipsum Has Been The Industry&#039;s Standard Dummy Text Ever Since</p>
+                        <p class="mb-0 mt-3"><?=$recentBlog->bc1?></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="blog-box">
-                    <div class="blog-box-thumb">
-                        <img src="asset/theme1/images/blog/624d62471f5b51649238599.jpg" alt="image">
+                <div class="/blog-box">
+                    <div class="/blog-box-thumb">
+                        <img src="/asset/theme1/images/blog/624d62471f5b51649238599.jpg" alt="image">
                     </div>
-                    <div class="blog-box-content">
-                        <span class="blog-category">Crypto</span>
-                        <h3 class="title"><a href="blog/39/facere-asperiores-odio-id-porro.html">Facere asperiores
-                                odio id porro.</a>
+                    <div class="/blog-box-content">
+                        <span class="/blog-category">Crypto</span>
+                        <h3 class="title"><a href="/blog/39/facere-asperiores-odio-id-porro.html"><?=$recentBlog->bh2?></a>
                         </h3>
-                        <ul class="blog-meta">
+                        <ul class="/blog-meta">
                             <li><i class="fas fa-clock"></i> 1 year ago</li>
                             <li><i class="fas fa-comment"></i> 0 Comments</li>
                         </ul>
-                        <p class="mb-0 mt-3">Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting
-                            Industry. Lorem Ipsum Has Been The Industry&#039;s Standard Dummy Text Ever Since</p>
+                        <p class="mb-0 mt-3"><?=$recentBlog->bc2?></p>
                     </div>
                 </div>
             </div>
             <div class="col-md-6 col-lg-4">
-                <div class="blog-box">
-                    <div class="blog-box-thumb">
-                        <img src="asset/theme1/images/blog/624d626242e021649238626.jpg" alt="image">
+                <div class="/blog-box">
+                    <div class="/blog-box-thumb">
+                        <img src="/asset/theme1/images/blog/624d626242e021649238626.jpg" alt="image">
                     </div>
-                    <div class="blog-box-content">
-                        <span class="blog-category">Coinbase</span>
-                        <h3 class="title"><a href="blog/40/eligendi-distinctio-molestias-ducimus.html">Eligendi
-                                distinctio molestias ducimus.</a>
+                    <div class="/blog-box-content">
+                        <span class="/blog-category">Coinbase</span>
+                        <h3 class="title"><a href="/blog/40/eligendi-distinctio-molestias-ducimus.html"><?=$recentBlog->bh3?></a>
                         </h3>
-                        <ul class="blog-meta">
+                        <ul class="/blog-meta">
                             <li><i class="fas fa-clock"></i> 1 year ago</li>
                             <li><i class="fas fa-comment"></i> 0 Comments</li>
                         </ul>
-                        <p class="mb-0 mt-3">Lorem Ipsum Is Simply Dummy Text Of The Printing And Typesetting
-                            Industry. Lorem Ipsum Has Been The Industry&#039;s Standard Dummy Text Ever Since</p>
+                        <p class="mb-0 mt-3"><?=$recentBlog->bc3?></p>
                     </div>
                 </div>
             </div>
