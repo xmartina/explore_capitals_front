75e4a7903a5ea109e6070ab61169937001d087ef
xmartina <martinsprince126@gmail.com>
2023-12-12 13:27:21 +0100

    d

--- contents/home/recentBlog.php
+++ contents/home/recentBlog.php
@@ -8,10 +8,10 @@ class recentBlog
         $p = $this;
 
         $p->bh1 = 'Navigating the Crypto Boom: Key Trends in 2023';
-        $p->bc1 = 'Content: Explore rising altcoins, decentralized finance, and the latest regulatory developments reshaping the crypto landscape.';
+        $p->bc1 = 'Explore rising altcoins, decentralized finance, and the latest regulatory developments reshaping the crypto landscape.';
         $p->bh2 = "The Metaverse Wave: Crypto's Next Frontier";
-        $p->bc2 = 'Content: Uncover how blockchain technology is driving the evolution of the metaverse and its potential impact on crypto markets.';
+        $p->bc2 = 'Uncover how blockchain technology is driving the evolution of the metaverse and its potential impact on crypto markets.';
         $p->bh3 = 'Sustainability in Crypto: Green Initiatives Taking Root';
-        $p->bc3 = 'Content: Discover how the crypto industry is embracing eco-friendly solutions, from energy-efficient consensus algorithms to carbon offset initiatives.';
+        $p->bc3 = 'Discover how the crypto industry is embracing eco-friendly solutions, from energy-efficient consensus algorithms to carbon offset initiatives.';
     }
 }
