be21ac428c457d1a01a576fefcf559453e958a23
xmartina <martinsprince126@gmail.com>
2023-12-12 05:41:59 +0100

    d

---
+++ contents/home/faq.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace contents\home;
+
+class faq
+{
+    public function __construct(){
+        $p = $this;
+
+        $p->f1h = 'How do I get started with investing?';
+        $p->f1c =  'Explore our user-friendly platform, sign up, and choose your investment plan.';
+        $p->f2h =  'What is the minimum investment amount?';
+        $p->f2c =  'Our plans accommodate various budgets, starting with a minimum investment of $50.';
+        $p->f3h =  'How are profits calculated and distributed?';
+        $p->f3c =  'Profits are calculated based on your chosen plan and are distributed regularly.';
+        $p->f4h =  'Is my personal and financial information secure?';
+        $p->f4c =  'Yes, we employ robust security measures to safeguard your data and investments.';
+        $p->f5h =  'Can I withdraw my funds at any time?';
+        $p->f5c =  'Yes, you can withdraw your funds at any time according to the terms of your plan.';
+        $p->f6h =  'What support is available if I have questions?';
+        $p->f6c =  'Our customer support team is available to assist you with any queries or concerns.';
+    }
+}
--- home/faq.php
+++ home/faq.php
@@ -1,3 +1,7 @@
+<?php
+use contents\home\faq;
+$faq = new faq();
+?>
 <section class="s-pt-100 s-pb-100 dark-bg">
     <div class="container">
         <div class="row justify-content-center">
@@ -16,14 +20,13 @@
                         <h2 class="accordion-header" id="heading-1">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse1" aria-expanded="false" aria-controls="collapseSix">
-                                When can I deposit/withdraw from my Investment account?
+                                <?=$faq->f1h ?>
                             </button>
                         </h2>
                         <div id="collapse1" class="accordion-collapse collapse" aria-labelledby="heading-1"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fuga sit
-                                    architecto sint voluptas adipisci similique magnam iusto magni sequi?</p>
+                                <p> <?=$faq->f1c ?></p>
                             </div>
                         </div>
                     </div>
@@ -36,14 +39,13 @@
                         <h2 class="accordion-header" id="heading-2">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapseSix">
-                                How do I check my account balance?
+                                <?=$faq->f2h ?>
                             </button>
                         </h2>
                         <div id="collapse2" class="accordion-collapse collapse" aria-labelledby="heading-2"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestias fuga sit
-                                    architecto sint voluptas adipisci similique magnam iusto magni sequi?</p>
+                                <p> <?=$faq->f2c ?></p>
                             </div>
                         </div>
                     </div>
@@ -56,17 +58,13 @@
                         <h2 class="accordion-header" id="heading-3">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse3" aria-expanded="false" aria-controls="collapseSix">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                <?=$faq->f3h ?>
                             </button>
                         </h2>
                         <div id="collapse3" class="accordion-collapse collapse" aria-labelledby="heading-3"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Fugiat, obcaecati quasi. Adipisci sapiente, nesciunt officiis minus in pariatur
-                                    nam dolore dicta cupiditate! Ipsam dolores iusto consectetur sit, dolore
-                                    voluptatibus officia soluta doloremque tempora sint quas beatae! Sit est quos,
-                                    reprehenderit iste velit eum ex ullam cupiditate officia unde, facilis dolorum
-                                    ab quibusdam.</p>
+                                <p> <?=$faq->f3c ?></p>
                             </div>
                         </div>
                     </div>
@@ -79,17 +77,13 @@
                         <h2 class="accordion-header" id="heading-4">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse4" aria-expanded="false" aria-controls="collapseSix">
-                                Tenetur laudantium sed sequi pariatur nam dolore dicta
+                                <?=$faq->f4h ?>
                             </button>
                         </h2>
                         <div id="collapse4" class="accordion-collapse collapse" aria-labelledby="heading-4"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Fugiat, obcaecati quasi. Adipisci sapiente, nesciunt officiis minus in pariatur
-                                    nam dolore dicta cupiditate! Ipsam dolores iusto consectetur sit, dolore
-                                    voluptatibus officia soluta doloremque tempora sint quas beatae! Sit est quos,
-                                    reprehenderit iste velit eum ex ullam cupiditate officia unde, facilis dolorum
-                                    ab quibusdam.</p>
+                                <p> <?=$faq->f4c ?></p>
                             </div>
                         </div>
                     </div>
@@ -102,17 +96,13 @@
                         <h2 class="accordion-header" id="heading-5">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse5" aria-expanded="false" aria-controls="collapseSix">
-                                Ipsam dolores iusto consectetur sit, dolore voluptatibus officia.
+                                <?=$faq->f5h ?>
                             </button>
                         </h2>
                         <div id="collapse5" class="accordion-collapse collapse" aria-labelledby="heading-5"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Fugiat, obcaecati quasi. Adipisci sapiente, nesciunt officiis minus in pariatur
-                                    nam dolore dicta cupiditate! Ipsam dolores iusto consectetur sit, dolore
-                                    voluptatibus officia soluta doloremque tempora sint quas beatae! Sit est quos,
-                                    reprehenderit iste velit eum ex ullam cupiditate officia unde, facilis dolorum
-                                    ab quibusdam.</p>
+                                <p> <?=$faq->f5c ?></p>
                             </div>
                         </div>
                     </div>
@@ -125,17 +115,13 @@
                         <h2 class="accordion-header" id="heading-6">
                             <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse"
                                     data-bs-target="#collapse6" aria-expanded="false" aria-controls="collapseSix">
-                                Repellat, et sapiente? Nisi nemo, voluptate voluptates unde molestias.
+                                <?=$faq->f6h ?>
                             </button>
                         </h2>
                         <div id="collapse6" class="accordion-collapse collapse" aria-labelledby="heading-6"
                              data-bs-parent="#accordionExample">
                             <div class="accordion-body">
-                                <p> Fugiat, obcaecati quasi. Adipisci sapiente, nesciunt officiis minus in pariatur
-                                    nam dolore dicta cupiditate! Ipsam dolores iusto consectetur sit, dolore
-                                    voluptatibus officia soluta doloremque tempora sint quas beatae! Sit est quos,
-                                    reprehenderit iste velit eum ex ullam cupiditate officia unde, facilis dolorum
-                                    ab quibusdam.</p>
+                                <p> <?=$faq->f6c ?></p>
                             </div>
                         </div>
                     </div>
